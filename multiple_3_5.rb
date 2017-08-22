9e6389707a037e593f7a5ea8ee95c639d5954b28
Tsutou <tsutoutakehara@gmail.com>
2017-08-22 20:10:12 +0900

    Multiple 3or5 < 1000

---
+++ multiple_3_5.rb
@@ -0,0 +1,21 @@
+
+def multiple_3_5
+
+  num = 0
+  sum = 0
+
+  while num < 999 do
+
+    num += 1
+
+    if num % 3 == 0 || num % 5 == 0
+      sum = sum + num
+    end
+
+  end
+
+puts sum
+
+end
+
+multiple_3_5
